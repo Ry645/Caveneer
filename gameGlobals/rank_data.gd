f94aeb5b2f6b01e13dd99fbcb009d4afe0730852
Ryan Sexton <ry645.dev@gmail.com>
2024-11-27 00:43:02 -0500

    created ranks from a base playthrough and basic math ratios

--- gameGlobals/rank_data.gd
+++ gameGlobals/rank_data.gd
@@ -1,6 +1,23 @@
 extends Node
 
-const 
+# I finished the game in 06:07.903
 
+## in milliseconds
+const timeToRank:Dictionary = {
+	320_000: "S",
+	360_000: "A",
+	450_000: "B",
+	640_000: "C",
+	-1: "D",
+}
+
+## in milliseconds
 func getRankFromTime(time:int) -> String:
-	return ""
+	for key in timeToRank.keys():
+		if time < key:
+			return timeToRank[key]
+	return timeToRank[-1]
+
+## converts seconds to milliseconds
+func secToMilliTime(a:float) -> int:
+	return int(a * 1000.0)
